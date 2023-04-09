7d0ec57f00d9b13069b9efaa09689cb14d92fa65
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-04-09 18:25:28 +0200

    Variable name changed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const wrapperQuery = document.querySelector(".wrapper")
 const squareQuerySelect = wrapperQuery.querySelectorAll(".square")
-squareQuerySelect.forEach(square=> square.addEventListener("click", issou)) //QuerySelectorAll returns a nodeList so we need to use forEach
-function issou()
+squareQuerySelect.forEach(square=> square.addEventListener("click", toggleExample)) //QuerySelectorAll returns a nodeList so we need to use forEach
+function toggleExample()
 {
     this.classList.toggle("red")
 }
