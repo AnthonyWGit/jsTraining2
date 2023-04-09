b948b7f8f7256031cfe53d46d366d307f43da23e
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-04-09 16:53:36 +0200

    Quick comment why we Foreach

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const wrapperQuery = document.querySelector(".wrapper")
 const squareQuerySelect = wrapperQuery.querySelectorAll(".square")
-squareQuerySelect.forEach(square=> square.addEventListener("click", issou))
+squareQuerySelect.forEach(square=> square.addEventListener("click", issou)) //QuerySelectorAll returns a nodeList so we need to use forEach
 function issou()
 {
     this.classList.toggle("red")
